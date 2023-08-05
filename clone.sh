8579d63c40479ddba33408c3d2779c32e86e7697
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-05 16:35:05 -0400

    Adding token to env file

--- clone.sh
+++ clone.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-source ~/bin/token.sh
-
-if [ -z "$1" ]; then
-
-echo "No repository given."
-
-else
-  
-  git clone "https://$token@github.com/$username/$1"
+source .env
+# Ensure the required environment variables are set
+if [[ -z "$GITHUB_TOKEN" || -z "$GITHUB_USERNAME" ]]; then
+  echo "Error: GitHub token or username not provided."
+  exit 1
 fi
+
+if [[ -z "$1" ]]; then
+  echo "Error: No repository name provided."
+  exit 1
+fi
+
+REPO_NAME="$1"
+
+# Validate repository name (this is a simple example, you might want more comprehensive validation)
+if [[ ! "$REPO_NAME" =~ ^[a-zA-Z0-9_.-]+$ ]]; then
+  echo "Error: Invalid repository name."
+  exit 1
+fi
+
+# Clone the repository
+echo "Cloning repository: $REPO_NAME"
+git clone "https://$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/$REPO_NAME"
+
