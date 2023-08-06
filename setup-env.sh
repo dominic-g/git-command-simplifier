5b66f63a2ab9ab92f9deeef323d7479575dd2494
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-06 05:32:41 -0400

    updating the colors and colored function on the common .env file

--- setup-env.sh
+++ setup-env.sh
@@ -14,4 +14,20 @@ DEST_DIR="$HOME/.github_app"
 echo "GITHUB_TOKEN=\"$GITHUB_TOKEN\"" > "$DEST_DIR/.env"
 echo "GITHUB_USERNAME=\"$GITHUB_USERNAME\"" >> "$DEST_DIR/.env"
 
+# Append color variables and function to the .env file
+echo -e "
+# Define ANSI color escape codes
+GREEN=\"\\033[0;32m\"
+WHITE=\"\\033[1;37m\"
+RED=\"\\033[1;31m\"
+BLUE=\"\\033[0;34m\"
+ORANGE=\"\\033[0;33m\"
+RESET=\"\\033[0m\"
+
+# Define a function to print colored messages
+print_colored_message() {
+  echo -e \"\$1\$2\$RESET\"
+}
+" >> "$DEST_DIR/.env"
+
 echo "Environment variables saved to .env file."
