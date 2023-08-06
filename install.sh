57a35cd49046e7e6b99be97e32f4fda5b51699e9
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-06 06:10:07 -0400

    Update the setting variable functionality in a function to avoid code repetition in install and set env scripts

--- install.sh
+++ install.sh
@@ -1,66 +1,42 @@
 #!/bin/bash
+GREEN="\033[0;32m"
+WHITE="\033[1;37m"
+RED="\033[1;31m"
+BLUE="\033[0;34m"
+ORANGE="\033[0;33m"
+RESET="\033[0m"
 
 # Destination directory
 DEST_DIR="$HOME/.github_app"
 
 # Check if the directory already exists
 if [ -d "$DEST_DIR" ]; then
-    echo "The folder is already set up at $DEST_DIR."
+    printf "${ORANGE}The folder is already set up at $DEST_DIR.${RESET}\n"
     exit 1
 fi
 
 # Create destination directory
-mkdir -p "$DEST_DIR" || { echo "Failed to create directory $DEST_DIR"; exit 1; }
+mkdir -p "$DEST_DIR" || { printf "${RED}Failed to create directory $DEST_DIR${RESET}\n"; exit 1; }
 
 # Copy the files
-cp clone.sh pu.sh pull.sh setup-env.sh "$DEST_DIR/" || { echo "Failed to copy files"; exit 1; }
+cp clone.sh pu.sh pull.sh setup-env.sh "$DEST_DIR/" || { printf "${RED}Failed to copy files${RESET}\n"; exit 1; }
 
 # Make the copied files executable
-chmod +x "$DEST_DIR/clone.sh" "$DEST_DIR/pu.sh" "$DEST_DIR/pull.sh" "$DEST_DIR/setup-env.sh" || { echo "Failed to make files executable"; exit 1; }
+chmod +x "$DEST_DIR/clone.sh" "$DEST_DIR/pu.sh" "$DEST_DIR/pull.sh" "$DEST_DIR/setup-env.sh" || { printf "${RED}Failed to make files executable${RESET}\n"; exit 1; }
 
 # Create symbolic links
-ln -s "$DEST_DIR/clone.sh" "$DEST_DIR/clone" || { echo "Failed to create symbolic link for clone"; exit 1; }
-ln -s "$DEST_DIR/pu.sh" "$DEST_DIR/push" || { echo "Failed to create symbolic link for push"; exit 1; }
-ln -s "$DEST_DIR/pull.sh" "$DEST_DIR/pull" || { echo "Failed to create symbolic link for pull"; exit 1; }
-ln -s "$DEST_DIR/setup-env.sh" "$DEST_DIR/github_token" || { echo "Failed to create symbolic link for github_token"; exit 1; }
+ln -s "$DEST_DIR/clone.sh" "$DEST_DIR/clone" || { printf "${ORANGE}Failed to create symbolic link for clone${RESET}\n"; printf "You will have to call the \"${BLUE}clone${RESET}\" command using \"${BLUE}clone.sh${RESET}\"\n"; }
+ln -s "$DEST_DIR/pu.sh" "$DEST_DIR/push" || { printf "${ORANGE}Failed to create symbolic link for push${RESET}\n"; printf "You will have to call the \"${BLUE}push${RESET}\" command using \"${BLUE}pu.sh${RESET}\"\n"; }
+ln -s "$DEST_DIR/pull.sh" "$DEST_DIR/pull" || { printf "${ORANGE}Failed to create symbolic link for pull${RESET}\n"; printf "You will have to call the \"${BLUE}pull${RESET}\" command using \"${BLUE}pull.sh${RESET}\"\n"; }
+ln -s "$DEST_DIR/setup-env.sh" "$DEST_DIR/github_token" || { printf "${ORANGE}Failed to create symbolic link for github_token${RESET}\n"; printf "You will have to call the \"${BLUE}github_token${RESET}\" command using \"${BLUE}setup-env.sh${RESET}\"\n"; }
 
 # Make the symbolic links executable
-chmod +x "$DEST_DIR/clone" "$DEST_DIR/push" "$DEST_DIR/pull" "$DEST_DIR/github_token" || { echo "Failed to make symbolic links executable"; exit 1; }
+chmod +x "$DEST_DIR/clone" "$DEST_DIR/push" "$DEST_DIR/pull" "$DEST_DIR/github_token" || { printf "${ORANGE}Failed to make symbolic links executable${RESET}"; printf "You will have to use the full script names as commands name\n"; }
 
-# Prompt user for GitHub token
-read -p "Enter your GitHub token " GITHUB_TOKEN
-
-# Validate the token using the given regexp
-if [[ ! "$GITHUB_TOKEN" =~ ^(gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$ ]]; then
-    echo "Error: Invalid GitHub token format."
-    exit 1
-fi
-
-# Prompt user for GitHub username
-read -p "Enter your GitHub username: " GITHUB_USERNAME
-
-# Store the token and username in the .env file
-echo "GITHUB_TOKEN=\"$GITHUB_TOKEN\"" > "$DEST_DIR/.env"
-echo "GITHUB_USERNAME=\"$GITHUB_USERNAME\"" >> "$DEST_DIR/.env"
-
-# Append color variables and function to the .env file
-echo -e "
-# Define ANSI color escape codes
-GREEN=\"\\033[0;32m\"
-WHITE=\"\\033[1;37m\"
-RED=\"\\033[1;31m\"
-BLUE=\"\\033[0;34m\"
-ORANGE=\"\\033[0;33m\"
-RESET=\"\\033[0m\"
-
-# Define a function to print colored messages
-print_colored_message() {
-  echo -e \"\$1\$2\$RESET\"
-}
-" >> "$DEST_DIR/.env"
+source "$DEST_DIR/setup-env.sh"
 
 # Add the destination directory to the PATH environment
-echo "export PATH=\$PATH:$DEST_DIR" >> "$HOME/.bashrc" || { echo "Failed to modify shell profile"; exit 1; }
+echo "export PATH=\$PATH:$DEST_DIR" >> "$HOME/.bashrc" || { printf "${RED}Failed to modify shell profile${RESET}\nYou will have to add ${BLUE}$DEST_DIR${RESET} to PATH Enviroment manualy. ${ORANGE}$HOME/.bashrc${RESET}\n"; exit 1; }
 
-echo "Installation completed. You may need to restart your terminal."
+printf "${GREEN}Installation completed.${RESET}\n ${BLUE}You may need to restart your terminal.${RESET}"
 
--- setup-env.sh
+++ setup-env.sh
@@ -1,33 +1,57 @@
 #!/bin/bash
+GREEN="\033[0;32m"
+WHITE="\033[1;37m"
+RED="\033[1;31m"
+BLUE="\033[0;34m"
+ORANGE="\033[0;33m"
+RESET="\033[0m"
 
-# Prompt user for GitHub token
-read -p "Enter your GitHub token: " GITHUB_TOKEN
-if [[ ! "$GITHUB_TOKEN" =~ ^(gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$ ]]; then
-    echo "Error: Invalid GitHub token format."
-    exit 1
-fi
-# Prompt user for GitHub username
-read -p "Enter your GitHub username: " GITHUB_USERNAME
 
-DEST_DIR="$HOME/.github_app"
-# Create or overwrite the .env file
-echo "GITHUB_TOKEN=\"$GITHUB_TOKEN\"" > "$DEST_DIR/.env"
-echo "GITHUB_USERNAME=\"$GITHUB_USERNAME\"" >> "$DEST_DIR/.env"
+setup_git_details() {
+    # Prompt user for GitHub token
+    read -p "Enter your GitHub token: " GITHUB_TOKEN
+    if [[ ! "$GITHUB_TOKEN" =~ ^(gh[ps]_[a-zA-Z0-9]{36}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$ ]]; then
+        printf "${RED}Error: Invalid GitHub token format.${RESET}"
+        exit 1
+    fi
+    # Prompt user for GitHub username
+    read -p "Enter your GitHub username: " GITHUB_USERNAME
 
-# Append color variables and function to the .env file
-echo -e "
-# Define ANSI color escape codes
-GREEN=\"\\033[0;32m\"
-WHITE=\"\\033[1;37m\"
-RED=\"\\033[1;31m\"
-BLUE=\"\\033[0;34m\"
-ORANGE=\"\\033[0;33m\"
-RESET=\"\\033[0m\"
+    # Prompt user for Git config email
+    read -p "Enter your Git config email: " GIT_CONFIG_EMAIL
+    if [[ ! "$GIT_CONFIG_EMAIL" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+        printf "${RED}Error: Invalid email format.${RESET}"
+        exit 1
+    fi
 
-# Define a function to print colored messages
-print_colored_message() {
-  echo -e \"\$1\$2\$RESET\"
+    # Prompt user for Git config name
+    read -p "Enter your Git config name: " GIT_CONFIG_NAME
+
+    DEST_DIR="$HOME/.github_app"
+
+    # Append color variables and function to the .env file
+    echo -e "
+    # Define ANSI color escape codes
+    GREEN=\"\\033[0;32m\"
+    WHITE=\"\\033[1;37m\"
+    RED=\"\\033[1;31m\"
+    BLUE=\"\\033[0;34m\"
+    ORANGE=\"\\033[0;33m\"
+    RESET=\"\\033[0m\"
+
+    # Define a function to print colored messages
+    print_colored_message() {
+    echo -e \"\$1\$2\$RESET\"
+    }
+    " > "$DEST_DIR/.env"
+
+    # Create or overwrite the .env file
+    echo "GITHUB_TOKEN=\"$GITHUB_TOKEN\"" >> "$DEST_DIR/.env"
+    echo "GITHUB_USERNAME=\"$GITHUB_USERNAME\"" >> "$DEST_DIR/.env"
+    echo "GITHUB_USERNAME=\"$GIT_CONFIG_EMAIL\"" >> "$DEST_DIR/.env"
+    echo "GITHUB_USERNAME=\"$GIT_CONFIG_NAME\"" >> "$DEST_DIR/.env"
+
+    printf "${GREEN}Git variables saved to Enivronment.${RESET}"
 }
-" >> "$DEST_DIR/.env"
 
-echo "Environment variables saved to .env file."
+setup_git_details()
