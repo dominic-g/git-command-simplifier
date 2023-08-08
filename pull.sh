ea0f3602dd715beaa0da49aa4d5afd81bdeef479
Dominic Gitau <dominicnjroge1@gmail.com>
2023-08-08 15:41:49 -0400

    Updating the pull request to anicipate usrname and password request

--- pull.sh
+++ pull.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-git pull
+source .env
+
+expect<<EOF
+spawn git pull
+expect {
+  "*Username*" {
+    send "{$GITHUB_TOKEN}\r"
+    exp_continue
+  }
+  "*Password*" {
+    send "{$GITHUB_TOKEN}\r"
+    exp_continue
+  }
+  "*error: " {
+    puts "ERROR: Git pull failed!"
+    exit 1
+  }
+  eof {
+    puts "Git pull completed successfully."
+  }
+}
+EOF
